8394843ed1b59c43e67a91466c251c1ced083fe7
mikixing <mikixing@qq.com>
2019-10-22 15:58:52 +0800

    替换http为request

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-var http = require('http')
-var https = require('https')
+var request = require('request')
 var fs = require('fs')
 var cheerio = require('cheerio')
 var Promise = require("bluebird");
@@ -7,32 +6,39 @@ const wz = 'https://image.baidu.com/search/index?tn=baiduimage&ipn=r&ct=20132659
 
 var strHtml = ''
 var results = []
-
-https.get(wz, function(res) {
-    res.on('data', function(chunk) {
-        strHtml += chunk
+var imageData = ''
+request.get(wz).on('data', function(chunk) {
+    strHtml += chunk
+}).on('end', function () {
+    // var $ = cheerio.load(strHtml)
+    strHtml.replace(/\"thumbURL\"\:\s*\"(.*?)\"/g, function ($0, $1) {
+        results.push($1)
+        return $0
     })
-    res.on('end', function () {
-        // var $ = cheerio.load(strHtml)
-        strHtml.replace(/\"thumbURL\"\:\s*\"(.*?)\"/g, function ($0, $1) {
-            results.push($1)
-            return $0
-        })
-        console.log(results)
-        results.slice(0, 10).forEach(url => {
-            saveImage(url)
-        })
-        // Promise.map(results, saveImage, {concurrency: 1})
+    console.log(results)
+    results.slice(0, 10).forEach(url => {
+        saveImage(url)
     })
+    // Promise.map(results, saveImage, {concurrency: 1})
 })
 
 function saveImage(imageUrl){
-    https.get(imageUrl, fn);
+    request.get(imageUrl).on('data',function(data){  //图片加载到内存变量
+        imageData += data;
+    }).on('end',function(){        //加载完毕保存图片
+        if(!fs.existsSync("./images")){
+            fs.mkdirSync("./images");
+        }
+        fs.writeFile('images/'+Math.random()+'.png',imageData,'binary',function (err) {  //以二进制格式保存
+            if(err) throw err;
+            console.log('保存成功');
+        });
+    });
 }
 
 
 var fn = function (res) {
-    res.setEncoding('binary');      //二进制(binary)
+    // res.setEncoding('binary');      //二进制(binary)
 
     var imageData ='';
     res.on('data',function(data){  //图片加载到内存变量
